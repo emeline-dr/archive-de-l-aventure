acb3d7a4c14155686590135d0b9e05a46b92ef1e
drewwlg <waldbilligleandrew64@gmail.com>
2025-05-21 09:41:20 +0200

    ajout page de comms

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,24 @@
-import './index.css'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
-import {
-  createRouter,
-  RouterProvider,
-} from '@tanstack/react-router';
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-import { Route as RootRoute } from './routes/__root';
+import { Route as RootRoute } from "./routes/__root";
 
-import { homeRoute } from './routes/home';
-import { loginRoute } from './routes/login';
-import { signInRoute } from './routes/signIn';
-import { indexRoute } from './routes';
-import { myCharactersRoute } from './routes/myCharacters';
-import { registersRoute } from './routes/registers';
-import { NewSheetRoute } from './routes/newSheet';
-import { CharactersOwnedRoute } from './routes/charactersOwned';
-import { OthersCharactersRoute } from './routes/othersCharacters';
-import { UpdateSheetRoute } from './routes/updateSheets';
+import { homeRoute } from "./routes/home";
+import { loginRoute } from "./routes/login";
+import { signInRoute } from "./routes/signIn";
+import { indexRoute } from "./routes";
+import { myCharactersRoute } from "./routes/myCharacters";
+import { registersRoute } from "./routes/registers";
+import { NewSheetRoute } from "./routes/newSheet";
+import { CharactersOwnedRoute } from "./routes/charactersOwned";
+import { OthersCharactersRoute } from "./routes/othersCharacters";
+import { UpdateSheetRoute } from "./routes/updateSheets";
+import { commentsRoute } from "./routes/comments";
 
 const routeTree = RootRoute.addChildren([
   homeRoute,
@@ -32,18 +30,19 @@ const routeTree = RootRoute.addChildren([
   registersRoute,
   OthersCharactersRoute,
   NewSheetRoute,
-  UpdateSheetRoute
+  UpdateSheetRoute,
+  commentsRoute,
 ]);
 
 const router = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
   }
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
---
+++ src/routes/comments.tsx
@@ -0,0 +1,65 @@
+import { createRoute } from "@tanstack/react-router";
+import { Route as RootRoute } from "./__root";
+import Sidebar from "../components/sidebar";
+import BackgroundIcon from "../components/backgroundIcon";
+
+export const commentsRoute = createRoute({
+  path: "/comments",
+  getParentRoute: () => RootRoute,
+  component: () => (
+    <div className="min-h-screen text-[#2c2c2c] font-uncial-antiqua relative">
+      <div className="flex">
+        <Sidebar></Sidebar>
+
+        <main className="flex-1 p-8 px-20">
+          <div className="flex items-center gap-2 text-sm text-[#2b2c2b] font-semibold">
+            <button className="bg-[#2b2c2b] text-[#6cf7d4]  rounded-full breadcrumb">
+              Les registres
+            </button>
+
+            <button className="bg-[#2b2c2b] text-[#6cf7d4]  rounded-full breadcrumb">
+              Fiche de Arlahne
+            </button>
+
+            <button className="bg-[#2b2c2b] text-white  rounded-full breadcrumb">
+              Commentaires
+            </button>
+          </div>
+
+          <h2 className="text-2xl font-bold mt-6 mb-2 underline">
+            Commentaires de la fiche de Arlahne
+          </h2>
+          <p className="text-right text-sm italic mb-4">Le 09/05/2025, 23:57</p>
+
+          <div className="mb-6">
+            <h3 className="text-xl font-bold mb-1">Kymeria</h3>
+            <div className="bg-[#e8dcc1] p-4 rounded text-sm leading-relaxed">
+              Nullam at dui ac nunc laoreet euismod sit amet non diam. Vivamus
+              viverra eu lectus ac tristique. Donec auctor pretium dignissim.
+              Praesent vestibulum consectetur magna, eget iaculis massa
+              vestibulum eu. Nulla elit elit, mattis sit amet est id, blandit
+              ullamcorper lacus. Curabitur lacinia semper massa id sagittis.
+              Phasellus consequat quam nec ligula hendrerit, ac facilisis tortor
+              elementum. Nunc sapien nisi, semper at purus quis, semper blandit
+              enim. Sed condimentum eu mauris id porttitor. Integer interdum ac
+              est dictum rutrum. Aliquam a turpis elementum, pretium nunc
+              congue, facilisis nibh.
+            </div>
+          </div>
+
+          <div className="bg-[#f9edcd] border border-[#e2c799] p-4 rounded">
+            <p className="text-sm text-gray-800">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
+              lobortis nisl cursus bibendum sit nulla accumsan sodales ornare.
+              At urna viverra non suspendisse neque, lorem. Pretium condimentum
+              pellentesque gravida id etiam sit sed arcu euismod. Rhoncus proin
+              orci duis scelerisque molestie cursus tincidunt aliquam.
+            </p>
+          </div>
+        </main>
+      </div>
+
+      <BackgroundIcon></BackgroundIcon>
+    </div>
+  ),
+});
