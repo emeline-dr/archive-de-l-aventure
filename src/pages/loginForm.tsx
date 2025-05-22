2c52acedf4e6a3cb322da393ff3c83dd35ba6829
Emeline D.R. <deroo.emeline@gmail.com>
2025-05-22 15:47:19 +0200

    Correction

--- src/pages/loginForm.tsx
+++ src/pages/loginForm.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useForm } from '@tanstack/react-form';
-import { useRouter, useRouterState } from '@tanstack/react-router';
 import Cookies from 'js-cookie';
 
 import type { AnyFieldApi } from '@tanstack/react-form';
@@ -17,10 +16,6 @@ function FieldInfo({ field }: { field: AnyFieldApi }) {
 }
 
 export function LoginForm() {
-    const router = useRouter();
-    const { search } = useRouterState();
-    const redirectTo = search.redirectTo ?? '/index';
-
     const [checked, setChecked] = useState(false);
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
@@ -58,9 +53,11 @@ export function LoginForm() {
                         expires: oneHourFromNow, path: '/',
                         secure: true
                     })
+
+                    console.log(data.jwt)
                 }
 
-                await router.navigate({ to: redirectTo });
+                window.location.href = "/index";
             } catch (error: unknown) {
                 if (error instanceof Error) {
                     console.error('Erreur de connexion:', error);
--- src/utils/authGuard.ts
+++ src/utils/authGuard.ts
@@ -1,17 +1,13 @@
 import { redirect } from '@tanstack/react-router';
 import Cookies from 'js-cookie';
-import { isJwtExpired } from './AuthUtils';
 
 export const requireAuth = () => {
     const token = Cookies.get('authToken');
 
-    if (!token || isJwtExpired(token)) {
+    if (!token) {
         Cookies.remove('authToken');
         throw redirect({
-            to: '/login',
-            search: {
-                redirectTo: window.location.pathname,
-            },
+            to: '/login'
         });
     }
 };
